c7a03e4552a5c30a9492022ffca6d2ec24ae8fc1
BruceAmende <bruceamende32@gmail.com>
2019-11-19 12:27:35 +0100

    loops

---
+++ loops/script.js
@@ -0,0 +1,11 @@
+function geefGetallen(){
+    var ingevuldGetal = document.getElementById("startgetal").value;
+    for(var teller = 1; teller <=10; teller+=1){
+       
+
+        console.log(teller * ingevuldGetal);
+
+
+
+    }
+}
